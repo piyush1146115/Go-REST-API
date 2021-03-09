1bd529d1141788a26f8b830db1abf26b568ce2f7
piyush1146115 <piyush@appscode.com>
2021-03-09 12:24:25 +0600

    C2: Added a simple JSON response to the API

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct{
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request){
+	articles := Articles{
+		Article{Title: "Test title", Desc: "Test Description", Content: "Hello World"},
+	}
+
+	fmt.Println("Endpoint Hit: All articles endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "Homepage Endpoint Hit")
@@ -12,6 +30,7 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func handleRequests(){
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 func main() {
