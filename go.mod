32dd0c49a6ab4a30cec4ba328339d0b30b109f99
piyush1146115 <piyush@appscode.com>
2021-03-10 11:00:21 +0600

    C6: Get started with Routers

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/piyush1146115/Go-REST-API
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strconv"
 	"sync"
+	"github.com/gorilla/mux"
 )
 
 var counter int
@@ -23,14 +24,22 @@ type Articles []Article
 func allArticles(w http.ResponseWriter, r *http.Request){
 	articles := Articles{
 		Article{Title: "Test title", Desc: "Test Description", Content: "Hello World"},
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 
 	fmt.Println("Endpoint Hit: All articles endpoint")
 	json.NewEncoder(w).Encode(articles)
 }
 
+//func returnAllArticles(w http.ResponseWriter, r *http.Request){
+//	fmt.Println("Endpoint Hit: returnAllArticles")
+//	json.NewEncoder(w).Encode(articles)
+//}
+
 func homePage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Homepage Endpoint Hit")
+	fmt.Fprintf(w, "Welcome to the Homepage!")
+	fmt.Println(w, "Homepage Endpoint Hit")
 	//http.ServeFile(w, r, r.URL.Path[1:])
 }
 
@@ -43,15 +52,24 @@ func incrementCounter(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests(){
 	//http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-	http.HandleFunc("/increment", incrementCounter)
+	//http.HandleFunc("/articles", allArticles)
+	//http.HandleFunc("/increment", incrementCounter)
+	//log.Fatal(http.ListenAndServe(":10000", nil))
+
 	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	//	http.ServeFile(w, r, r.URL.Path[1:])
 	//})
-	http.Handle("/", http.FileServer(http.Dir("./Static")))
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	//http.Handle("/", http.FileServer(http.Dir("./Static")))
+	//log.Fatal(http.ListenAndServe(":8081", nil))
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 func main() {
 //fmt.Println("Hello")
+	fmt.Println("Rest API v2.0 - Mux Routers")
 	handleRequests()
 }
